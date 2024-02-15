cfbe445c206fe2c586e35702e35cdce4e9e34fbf
PGamingHD <pontus.2003@hotmail.com>
2024-02-15 15:02:51 +0100

    Added auth included in base files for Blazor.
    Added auth included in base files for Blazor. Base has been published and is ready to be developed with.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace OPC5_BlogApp.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+    }
+}
---
+++ Data/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace OPC5_BlogApp.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+}
