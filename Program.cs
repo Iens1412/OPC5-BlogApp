11cc8997f20515407e88b6203dc2bdc9d090d7af
PGamingHD <pontus.2003@hotmail.com>
2024-02-15 15:06:34 +0100

    Removed HTTPS and moved over to HTTP connections
    Removed config for HTTPS and moved over to HTTP connections

--- Program.cs
+++ Program.cs
@@ -51,11 +51,7 @@ namespace OPC5_BlogApp
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
-
-            app.UseHttpsRedirection();
 
             app.UseStaticFiles();
             app.UseAntiforgery();
