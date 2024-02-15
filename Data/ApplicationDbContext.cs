d24956683d5687c124285c6431a872a3beabee95
PGamingHD <pontus.2003@hotmail.com>
2024-02-15 15:01:18 +0100

    Removed files, to add new files but auth included!
    Removed files, to add new files but auth included!

--- Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace OPC5_BlogApp.Data
-{
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
-}
