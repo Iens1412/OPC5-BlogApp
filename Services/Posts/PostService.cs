54bc527ef82d3ce9ba5a420c245027b2c5f96575
PGamingHD <pontus.2003@hotmail.com>
2024-02-28 21:16:22 +0100

    Added post comments and fixed reloading comment amount on new comment
    Added post comments count and fixed reloading comment amount on new comment

--- Services/Posts/PostService.cs
+++ Services/Posts/PostService.cs
@@ -48,7 +48,7 @@ namespace Services.Posts
 
         public async Task<List<Post>> GetPosts(int count)
         {
-            return await context.Posts.Include(p => p.User)
+            return await context.Posts.Include(p => p.User).Include(p => p.PostComments)
                 .OrderByDescending(p => p.PostId)
                 .Take(count)
                 .ToListAsync();
